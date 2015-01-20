804d81b20572a9ab8f83638db2105e217bc87a0b
bttf <adnanchowdhury88@gmail.com>
2015-01-20 00:04:44 -0500

    responsive sidebar

---
+++ app/models/entry.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+var attr = DS.attr;
+
+export default DS.Model.extend({
+  title: attr('string'),
+  content: attr('string'),
+  tags: attr(),
+  created: attr('date'),
+  slug: attr('string'),
+  isFilm: attr('boolean'),
+  isEssay: attr('boolean')
+});
